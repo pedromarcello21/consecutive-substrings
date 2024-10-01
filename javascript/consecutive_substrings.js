e0210930ac81fafe8bd6652bb815eedb734d09bc
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-10-01 10:00:32 -0400

    solution

--- javascript/consecutive_substrings.js
+++ javascript/consecutive_substrings.js
@@ -1,5 +1,16 @@
 function consecutiveSubstrings(string) {
   // type your code here
+  //define list
+  const substrings = []
+  //loop thru each value
+  for (let i = 0; i < string.length; i++) {
+    //loop thru the next value
+    for (let j = i + 1; j <= string.length; j++) {
+      //push the string from value i to j onto the defined list
+      substrings.push(string.slice(i, j));
+    }
+  }
+  return substrings
 }
 
 if (require.main === module) {
